600f3d15d633778b10f5d3d11196db7c223f30de
fpresta0607 <82167077+fpresta0607@users.noreply.github.com>
2022-11-21 19:31:52 -0600

    tuned pressure and temp measurements

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ USBSerial ser;
 Thread tempthread;
 int16_t AC1, AC2, AC3, B1, B2, MB, MC, MD;;
 uint16_t AC4, AC5, AC6;
-int32_t UP,UT,B5,TOUT = 0,B6,X1,X2,X3,B3,p;
-uint32_t B4,B7;
+int64_t UP,UT,B5,TOUT = 0,B6,X1,X2,X3,B3,p;
+uint64_t B4,B7;
 
 void measure_temp(){
     I2C sensor_bus(I2C_SDA0,I2C_SCL0);
@@ -220,11 +220,30 @@ void measure_temp(){
         X1 = (UT-AC6)*AC5/pow(2,15);
         X2 = MC*pow(2,11)/(X1+MD);
         B5 = X1+X2;
-        TOUT = (B5 + 8)/pow(2,4);
+        TOUT = ((B5 + 8)/pow(2,4)) - 41;
         ser.printf("Calibrated temperature: %d C\n\r",TOUT);
 
         //Calculate true pressure
-        
+        B6 = B5 - 4000;
+        X1 = (B2 * (B6*B6/pow(2,12))) / pow(2,11);
+        X2 = AC2 * B6 / pow(2,11);
+        X3 = X1 + X2;
+        B3 = (((AC1 * 4 + X3) << oss)+2)/4;
+        X1 = AC3 * B6 / pow(2,13);
+        X2 = (B1 * (B6 * B6 / pow(2,12))) / pow(2,16);
+        X3 = ((X1 + X2) + 2) / pow(2,2);
+        B4 = AC4 * (uint64_t)(X3 + 32768) / pow(2,15);
+        B7 = ((uint64_t)UP - B3) * (50000 >> oss);
+        if (B7 < 0x80000000){
+            p = (B7*2)/B4;
+        } else{
+            p = (B7/B4) * 2;
+        }
+        X1 = (p/pow(2,8))*(p/pow(2,8));
+        X1 = (X1*3038)/ pow(2,16);
+        X2 = (-7357*p)/pow(2,16);
+        p = (p + (X1 + X2 +3791) / pow(2,4)) + 1000;
+        ser.printf("Calibrated pressure: %d Pa\n\r",p);
         thread_sleep_for(500);
     }
         
